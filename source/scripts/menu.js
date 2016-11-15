5a63dadc012ab8cb1851e302864574cfdc88af07
Jimmy Letecheur <jimmy@letecheur.me>
2016-11-15 14:39:26 +0100

    code menu and menu responsive

---
+++ source/scripts/menu.js
@@ -0,0 +1,13 @@
+(function($){
+    $('.flexnav__dropbtn').click(function (e) {
+        e.preventDefault();
+        $(this).next().toggleClass('open');
+        $(this).toggleClass('unfolded');
+    })
+
+    $('.menu-button').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('menu-unfolded');
+        $('body').toggleClass('with-menu');
+    })
+})(jQuery);
