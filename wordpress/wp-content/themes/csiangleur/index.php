5b914a1f4162d109a1f1d55c3216354f7f4fdfc8
Jimmy Letecheur <letecheur.j@gmail.com>
2016-11-25 22:56:11 +0100

    init default files for theme csiangleur

---
+++ wordpress/wp-content/themes/csiangleur/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: Page d’accueil
+*/; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title><?php echo bloginfo(); ?></title>
+	<meta name="description" content="<?php /*echo the_field("meta-description"); */?>">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/assets/css/main.css'; ?>" type="text/css">
+	<?php wp_head(); ?>
+</head>
+<body class="no-js main">
+<header class="header">
+	
+
+
+
+<?php/* include 'footer.php';*/ ?>
+
