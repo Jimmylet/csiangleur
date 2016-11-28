faf0b00d5448b49c59e3197273d8a137859efad7
Jimmy Letecheur <letecheur.j@gmail.com>
2016-11-28 18:27:17 +0100

    integration others services with repeater

--- wordpress/wp-content/themes/csiangleur/serviceequipe.php
+++ wordpress/wp-content/themes/csiangleur/serviceequipe.php
@@ -85,155 +85,77 @@ Template Name: Services et équipe
 			</div>
 		</section>
 	</div>
+	
+	
+	<?php
+	if( have_rows('service_add') ):
+	while ( have_rows('service_add') ) : the_row(); ?>
+	
 	<section class="content content-2nd">
 		<div class="content__wrap">
 			<div class="content__info">
-				<div class="content__info-title"><img src="./images/svg/services_infirmiers.svg" alt="" width="57" height="51">
-					<h2>Soins infirmiers</h2>
+				<div class="content__info-title">
+					<img src="<?php echo $images . 'svg/services_';?><?php echo the_sub_field('service_icon') . '.svg'; ?>" alt="" width="57" height="51">
+					<h2><?php echo the_sub_field('service_name'); ?></h2>
 				</div>
-				<div class="content__info-horaire"><span class="content__info-horaire__title">Horaire du service&nbsp;:</span>
+				<?php if (!empty(get_sub_field('service_horaire'))): ?>
+				<div class="content__info-horaire">
+					<span class="content__info-horaire__title">Horaire du service&nbsp;:</span>
 					<table class="content__info-horaire__wrap">
 						<tbody>
-						<tr>
-							<td>Lundi - Vendredi</td>
-							<td>10:00 - 17:00</td>
-						</tr>
-						<tr>
-							<td>Samedi</td>
-							<td>9:00 - 12:00</td>
-						</tr>
+						<?php
+						if( have_rows('service_horaire') ):
+						while ( have_rows('service_horaire') ) : the_row(); ?>
+								<tr>
+									<td><?php echo the_sub_field('horaire_jours');?></td>
+									<td><?php echo the_sub_field('horaire_heures');?></td>
+								</tr>
+						<?php endwhile; endif; ?>
 						</tbody>
 					</table>
 				</div>
+				<?php endif; ?>
 			</div>
 			<div class="content__description">
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla.</p>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en.</p>
+				<?php echo the_sub_field('service_description'); ?>
 			</div>
 			<div class="equipe">
-				<figure><img src="http://lorempixel.com/192/124/people/1" alt="Photo de" width="240" height="124">
-					<figcaption><span class="equipe__name">Docteur Lorem</span><span class="equipe__role">Médecin généraliste</span>
-						<div class="equipe__dispo"><span class="equipe__dispo__clic">Voir les disponibilités</span>
-							<div class="equipe__dispo__horaire" id="dispoHoraire">
-								<table>
-									<tbody>
-									<tr>
-										<td>Lundi</td>
-										<td>9:00 - 19:00</td>
-									</tr>
-									<tr>
-										<td>Mardi</td>
-										<td>Non disponible</td>
-									</tr>
-									<tr>
-										<td>Mercredi</td>
-										<td>9:00 - 14:00</td>
-									</tr>
-									<tr>
-										<td>Jeudi</td>
-										<td>12:00 - 17:00</td>
-									</tr>
-									<tr>
-										<td>Vendredi</td>
-										<td>10:00 - 19:00</td>
-									</tr>
-									<tr>
-										<td>Samedi</td>
-										<td>Non disponible</td>
-									</tr>
-									<tr>
-										<td>Dimanche</td>
-										<td>Non disponible</td>
-									</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</figcaption>
-				</figure>
-				<figure><img src="http://lorempixel.com/192/124/people/2" alt="Photo de" width="240" height="124">
-					<figcaption><span class="equipe__name">Docteur Lorem</span><span class="equipe__role">Médecin généraliste</span>
-						<div class="equipe__dispo"><span class="equipe__dispo__clic">Voir les disponibilités</span>
-							<div class="equipe__dispo__horaire" id="dispoHoraire">
-								<table>
-									<tbody>
-									<tr>
-										<td>Lundi</td>
-										<td>9:00 - 19:00</td>
-									</tr>
-									<tr>
-										<td>Mardi</td>
-										<td>Non disponible</td>
-									</tr>
-									<tr>
-										<td>Mercredi</td>
-										<td>9:00 - 14:00</td>
-									</tr>
-									<tr>
-										<td>Jeudi</td>
-										<td>12:00 - 17:00</td>
-									</tr>
-									<tr>
-										<td>Vendredi</td>
-										<td>10:00 - 19:00</td>
-									</tr>
-									<tr>
-										<td>Samedi</td>
-										<td>Non disponible</td>
-									</tr>
-									<tr>
-										<td>Dimanche</td>
-										<td>Non disponible</td>
-									</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</figcaption>
-				</figure>
-				<figure><img src="http://lorempixel.com/192/124/people/1" alt="Photo de" width="240" height="124">
-					<figcaption><span class="equipe__name">Docteur Lorem</span><span class="equipe__role">Médecin généraliste</span>
-						<div class="equipe__dispo"><span class="equipe__dispo__clic">Voir les disponibilités</span>
-							<div class="equipe__dispo__horaire" id="dispoHoraire">
-								<table>
-									<tbody>
-									<tr>
-										<td>Lundi</td>
-										<td>9:00 - 19:00</td>
-									</tr>
-									<tr>
-										<td>Mardi</td>
-										<td>Non disponible</td>
-									</tr>
-									<tr>
-										<td>Mercredi</td>
-										<td>9:00 - 14:00</td>
-									</tr>
-									<tr>
-										<td>Jeudi</td>
-										<td>12:00 - 17:00</td>
-									</tr>
-									<tr>
-										<td>Vendredi</td>
-										<td>10:00 - 19:00</td>
-									</tr>
-									<tr>
-										<td>Samedi</td>
-										<td>Non disponible</td>
-									</tr>
-									<tr>
-										<td>Dimanche</td>
-										<td>Non disponible</td>
-									</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</figcaption>
-				</figure>
+				<?php
+				if( have_rows('service_membres') ):
+					while ( have_rows('service_membres') ) : the_row(); ?>
+						<figure>
+							<?php $image = wp_get_attachment_image_src(get_sub_field('service_membre_photo')['id'], 'thumb-membre-equipe');
+							$imageinfos = get_sub_field('service_membre_photo'); ?>
+							<img src="<?php echo $image['0']; ?>" alt="<?php echo $imageinfos['alt']; ?>" width="<?php echo $image['1']; ?>" height="<?php echo $image['2']; ?>"/>
+							<figcaption>
+								<span class="equipe__name"><?php echo the_sub_field('service_membre_name');?></span>
+								<span class="equipe__role"><?php echo the_sub_field('service_membre_profession');?></span>
+								<div class="equipe__dispo">
+									<span class="equipe__dispo__clic">Voir les disponibilités</span>
+									<div class="equipe__dispo__horaire" id="dispoHoraire">
+										<table>
+											<tbody>
+											<?php
+											if( have_rows('service_membre_horaire') ):
+												while ( have_rows('service_membre_horaire') ) : the_row(); ?>
+													<tr>
+														<td><?php echo the_sub_field('service_membre_jour');?></td>
+														<td><?php echo the_sub_field('service_membre_heures');?></td>
+													</tr>
+												<?php endwhile; endif; ?>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</figcaption>
+						</figure>
+					<?php endwhile; endif; ?>
 			</div>
 		</div>
 	</section>
+	
+	<?php endwhile; endif; ?>
+
 </main>
 
 <?php include 'footer.php'; ?>
