86407eb663d015ec476e812ff5c03b26b45ab0c0
Jimmy Letecheur <letecheur.j@gmail.com>
2016-11-29 20:41:51 +0100

    modify function.php for wp-caption

--- wordpress/wp-content/themes/csiangleur/functions.php
+++ wordpress/wp-content/themes/csiangleur/functions.php
@@ -54,3 +54,35 @@ function cc_mime_types($mimes) {
 	return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+// Modify attribut wp-caption
+add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+add_shortcode('caption', 'fixed_img_caption_shortcode');
+function fixed_img_caption_shortcode($attr, $content = null) {
+	if ( ! isset( $attr['caption'] ) ) {
+		if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+			$content = $matches[1];
+			$attr['caption'] = trim( $matches[2] );
+		}
+	}
+	
+	$output = apply_filters('img_caption_shortcode', '', $attr, $content);
+	if ( $output != '' )
+		return $output;
+	
+	extract(shortcode_atts(array(
+		'id' => '',
+		'align' => 'alignnone',
+		'width' => '',
+		'caption' => ''
+	), $attr));
+	
+	if ( 1 > (int) $width || empty($caption) )
+		return $content;
+	
+	if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+	
+	return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="max-width: ' . $width . 'px">' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
+
+// Change articles name
